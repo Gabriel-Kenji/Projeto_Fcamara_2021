24e9199a0db2c2f6f27c0bcdd619be20673a4f08
Gabriel Kenji Utiyama <68922914+Gabriel-Kenji@users.noreply.github.com>
2021-03-23 18:23:28 -0300

    Base do código

---
+++ _api/cadastro.php
@@ -0,0 +1,48 @@
+<?php
+require __DIR__ . "/../vendor/autoload.php";
+session_start();
+if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) && !empty($_POST['senha']) && isset($_POST['senhaconf']) && !empty($_POST['senhaconf']) && isset($_POST['nome']) && !empty($_POST['nome'])){
+  
+    $user = new \_api\Classes\Usuario();
+    $usuario = $_POST['nome'];
+    $senha = $_POST['senha'];
+    $senhaconf = $_POST['senhaconf'];
+    $email = $_POST['email'];
+    $user->setEmail($email);
+    $user->setNome($usuario);
+    $user->setSenha($senha);
+   
+
+    if($senha == $senhaconf){
+        $cadastroDao = new \_api\Classes\CadastroDao();
+        $cadastroDao->read($user);
+        if($cadastroDao->read($user) == FALSE){
+            $cadastroDao->create($user);
+            $_SESSION['erro_cad'] = 4;
+            echo "foi";
+            //header("Location: ../index.php");
+        }
+        else{
+            //ERRO EMAIL JA EXISTENTE
+            echo "email";
+            $_SESSION['erro_cad'] = 3;
+            //header("Location: ../index.php");
+        }
+    }
+    else
+    {
+        //ERRO SENHAS DIFERENTES
+        echo "senha";
+        $_SESSION['erro_cad'] = 2;
+        //header("Location: ../index.php");
+    }
+}
+else
+{
+    echo "vazio";
+    $_SESSION['erro_cad'] = 1;
+    //header("Location: ../index.php");
+}
+
+
+?>
