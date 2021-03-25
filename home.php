cd7c7b5088c71954f2aa8f6f12e5bb62ff561a4f
Rodrigo Lopes <49988854+lsRodrigoV@users.noreply.github.com>
2021-03-25 04:42:13 -0300

    Verificando se o usuario esta logado.

--- home.php
+++ home.php
@@ -1,3 +1,18 @@
+<?php
+require_once 'conexao.php';
+
+session_start(); 
+
+//pegando os dados
+$id = $_SESSION['idusuario'];
+
+//verificacao
+
+if(!isset($_SESSION['logado']))
+header("Location: /projeto_fcamara_2021/index.html");
+
+?>
+
 <html>
 <head>
     <title> Logado </title>
