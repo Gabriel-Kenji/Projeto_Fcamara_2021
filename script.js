74c9975177c45c0d872d99848fec9894bc6978bf
jonathanferreira33 <jhouus33@gmail.com>
2021-03-30 09:02:23 -0300

    Modal de login e cadastro

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLogin() {
+    document.getElementById("btnLogin").style.top = '0';
+}
+
+function closeLogin() {
+    document.getElementById("btnLogin").style.top = '-100%';
+}
+
+function alternarForm() {
+    var container = document.querySelector(".container-modal");
+    container.classList.toggle('active')
+}
