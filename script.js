03085f93f41c0ee4494145d5dee1f0c6b521f1af
jonathanferreira33 <jhouus33@gmail.com>
2021-04-01 19:47:12 -0300

    Add modal cadastro com check tipos de usuarios

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function openLogin() {
     document.getElementById("btnLogin").style.top = '0';
+    var container = document.querySelector(".container-modal");
+    container.classList.remove('active')
 }
 
 function closeLogin() {
-    document.getElementById("btnLogin").style.top = '-100%';
+    document.getElementById("btnLogin").style.top = '-150%';
 }
 
-// function alternarForm() {
-//     var container = document.querySelector(".container-modal");
-//     container.classList.toggle('active')
-// }
+function alternarForm() {
+    console.log('ok')
+    var container = document.querySelector(".container-modal");
+    container.classList.toggle('active')
+}
+
+function openCadastro() {
+    document.getElementById("btnLogin").style.top = '0';
+    var container = document.querySelector(".container-modal");
+    container.classList.add('active')
+}
