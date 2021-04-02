a28ed541925d70c01ad8b2a60ab7c3d04f3c3c0b
Gabriel Kenji Utiyama <68922914+Gabriel-Kenji@users.noreply.github.com>
2021-04-02 19:41:50 -0300

    Atualização no Banco de Dados
    Foi acrescentado as tabelas de status de pedido e doação. E foi feita uma breve populada no banco

--- banco_fcamara_2021/db_fcamara.sql
+++ banco_fcamara_2021/db_fcamara.sql
@@ -1,3 +1,4 @@
+
 create database db_fcamara;
 
 use db_fcamara;
@@ -33,13 +34,20 @@ CREATE TABLE tb_material (
     nm_material varchar(255) NOT NULL,
     PRIMARY KEY (cd_material)
 );
+CREATE TABLE tb_status_pedido (
+    cd_status int NOT NULL AUTO_INCREMENT,
+    nm_status varchar(255) NOT NULL,
+    PRIMARY KEY (cd_status)
+);
 
 CREATE TABLE tb_pedido (
     cd_pedido int NOT NULL AUTO_INCREMENT,
-    ds_status varchar(255) NOT NULL,
+    cd_status int NOT NULL,
     cd_material int NOT NULL,
+    ds_quantidade int NOT NULL,
     cd_RA int NOT NULL,
     PRIMARY KEY (cd_pedido),
+    FOREIGN KEY (cd_status) REFERENCES tb_status_pedido(cd_status),
     FOREIGN KEY (cd_RA) REFERENCES tb_aluno(cd_RA),
     FOREIGN KEY (cd_material) REFERENCES tb_material(cd_material)
 );
@@ -56,13 +64,61 @@ CREATE TABLE tb_doador (
     PRIMARY KEY (cd_CPF)
 );
 
-CREATE TABLE tb_doacao (
+CREATE TABLE tb_status_doacao (
+    cd_status int NOT NULL AUTO_INCREMENT,
+    nm_status varchar(255) NOT NULL,
+    PRIMARY KEY (cd_status)
+);
+
+CREATE TABLE tb_doacao(
     cd_doacao int NOT NULL AUTO_INCREMENT,
-    ds_status varchar(255) NOT NULL,
+    cd_status int NOT NULL,
     cd_CPF int NOT NULL,
+    ds_quantidade int NOT NULL,
     cd_pedido int NOT NULL,
     PRIMARY KEY (cd_doacao),
+    FOREIGN KEY (cd_status) REFERENCES tb_status_doacao(cd_status),
     FOREIGN KEY (cd_CPF) REFERENCES tb_doador(cd_CPF),
     FOREIGN KEY (cd_pedido) REFERENCES tb_pedido(cd_pedido)
 );
+
+INSERT INTO `tb_status_pedido` (`cd_status`, `nm_status`) VALUES
+(1, 'EM ANDAMENTO'),
+(2, 'CONCLUÍDO ');
+
+INSERT INTO `tb_status_doacao` (`cd_status`, `nm_status`) VALUES
+(1, 'EM ANDAMENTO'),
+(2, 'CONCLUÍDO ');
+
+
+INSERT INTO `tb_material` (`cd_material`, `nm_material`) VALUES 
+('1', 'Apontador'),
+('2', 'Borracha escolar'),
+('3', 'Caderno 10 matérias'),
+('4', 'Caderno de caligrafia'),
+('5', 'Caderno 1 matérias'),
+('6', 'Canetinha hidrográfica (12 cores)'),
+('7', 'Cola branca 90g'),
+('8', 'Lápis de cor (12 cores)'),
+('9', 'Lápis grafite'),
+('10', 'Lapiseira'),
+('11', 'Caneta esferográfica'),
+('12', 'Tesoura sem ponta'),
+('13', 'Régua'),
+('14', 'Esquadro 45o'),
+('15', 'Esquadro 60o'),
+('16', 'Transferidor 180o'),
+('17', 'Pasta'),
+('18', 'Estojo'),
+('19', 'Mochila');
+
+INSERT INTO `tb_escola` (`cd_INEP`, `nm_escola`, `nm_email`, `nm_senha`, `sg_estado`, `nm_cidade`, `nm_bairro`, `ds_telefone`) VALUES
+('35909836', 'Vila Tupi', 'vila@tupi.com', '1234', 'SP', 'Praia Grande', 'Vila Tupi', '1399999999'),
+('35045196', 'Professora Magali Alonso', 'Magali@alonso.com', '1234', 'SP', 'Praia Grande', 'Vila Tupi', '1399999999'),
+('35008096', 'Doutor Luiz Lobo Neto', 'lobo@neto.com', '1234', 'SP', 'Santo André', 'Paraiso', '1199999999'),
+('35008059', 'Doutor Celso Gama', 'celso@gama.com', '1234', 'SP', 'Santo André', 'Vila Assunção', '1199999999');
+
+INSERT INTO `tb_aluno` (`cd_RA`, `nm_aluno`, `nm_email`, `nm_senha`, `sg_estado`, `nm_cidade`, `nm_bairro`, `ds_telefone`, `cd_INEP`) VALUES 
+('1', 'Gabriel Kenji', 'gabrielkenji@gmail.com', '1234', 'SP', 'Praia Grande', 'Vila Tupi', '13999999999', '35909836'), 
+('2', 'Rodrigo', 'rodrigo@gmail.com', '1234', 'SP', 'Praia Grande', 'Vila Tupi', '13999999999', '35045196') ;
 
